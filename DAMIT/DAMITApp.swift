a77bdf61140000fd49a56841e156afe07254d47b
Rostislav Brož <scraptechguy@gmail.com>
2025-06-23 17:37:40 +0200

    Initial Commit

---
+++ DAMIT/DAMITApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAMITApp.swift
+//  DAMIT
+//
+//  Created by Rostislav Brož on 23.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct DAMITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DAMITTests/DAMITTests.swift
@@ -0,0 +1,16 @@
+//
+//  DAMITTests.swift
+//  DAMITTests
+//
+//  Created by Rostislav Brož on 23.06.2025.
+//
+
+import Testing
+
+struct DAMITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
