a23d879a024eeb17db753345fbb222eb732604fb
Rostislav Brož <scraptechguy@gmail.com>
2025-07-09 14:42:26 +0200

    Add LaunchView and update app entry point
    Introduced a new LaunchView and set it as the main view in DAMITApp. Moved HomeView to a new directory for better organization.

---
+++ DAMIT/Views/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  DAMIT
+//
+//  Created by Rostislav Brož on 09.07.2025.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+#Preview {
+    LaunchView()
+}
