6bb6bfd4665702e58b63a70a84d95703b51dc16b
Rostislav Brož <scraptechguy@gmail.com>
2025-07-09 14:40:21 +0200

    Rename ContentView to HomeView and move to Views folder
    Renamed ContentView.swift to HomeView.swift, updated the struct name accordingly, and moved the file to the Views directory to better reflect its purpose and improve project organization.

--- DAMIT/Views/HomeView.swift
+++ DAMIT/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  DAMIT
 //
 //  Created by Rostislav Brož on 23.06.2025.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
